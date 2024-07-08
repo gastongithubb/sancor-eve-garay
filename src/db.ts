1b19ff4729b68104dec6635ead51da53a06d2198
Gaston <gastonalvarez18@outlook.com>
2024-07-08 02:50:16 -0300

    db

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,3 @@
-// db.js
-
 import express from 'express';
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
@@ -14,34 +12,24 @@ const pool = new Pool({
   host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DATABASE,
   password: process.env.POSTGRES_PASSWORD,
-  port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
+  port: parseInt(process.env.POSTGRES_PORT as string) || 5432,
   ssl: {
     rejectUnauthorized: false
   }
 });
 
-// Middleware para permitir solicitudes JSON
-app.use(express.json());
-
-// Endpoint para obtener métricas
 app.get('/api/metrics', async (req, res) => {
   try {
     console.log('Realizando consulta a la base de datos');
     const result = await pool.query('SELECT * FROM metrics');
-    console.log('Resultado de la consulta:', result.rows);
+    console.log('Resultado de la consulta:', result.rows); // Log para verificar los datos obtenidos
     res.json(result.rows);
   } catch (err) {
-    console.error('Error en la consulta a la base de datos:', err);
-    res.status(500).send('Error al cargar las métricas. Detalles en los logs.');
+    console.error('Error en la consulta a la base de datos:', err); // Log más detallado del error
+    res.status(500).send('Error al cargar las métricas. Por favor, intenta de nuevo más tarde.');
   }
 });
 
-// Manejo de errores 404 para rutas no encontradas
-app.use((req, res) => {
-  res.status(404).send('Ruta no encontrada');
-});
-
-// Iniciar servidor
 app.listen(port, () => {
   console.log(`Servidor Express corriendo en el puerto ${port}`);
 });
