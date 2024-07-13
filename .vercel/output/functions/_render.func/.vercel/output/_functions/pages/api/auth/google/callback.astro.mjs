4662127f2e8c7a8fd66043db21efbb094bca9723
Gaston <gastonalvarez18@outlook.com>
2024-07-13 13:21:57 -0300

    cambios

--- .vercel/output/functions/_render.func/.vercel/output/_functions/pages/api/auth/google/callback.astro.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../../../../renderers.mjs';
-
-const page = () => import('../../../../chunks/callback_ByX1UGoO.mjs');
-
-export { page };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/pages/notfound.astro.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('../chunks/notfound_DohxVlU5.mjs');
-
-export { page };
