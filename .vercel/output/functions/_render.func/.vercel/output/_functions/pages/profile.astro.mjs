6812b1efe393c3e0a5b83182221c1aab47acceaf
Gaston <gastonalvarez18@outlook.com>
2024-07-13 13:41:32 -0300

    se lemina commit

--- .vercel/output/functions/_render.func/.vercel/output/_functions/pages/profile.astro.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('../chunks/profile_DJ49HvHZ.mjs');
-
-export { page };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/pages/promotores.astro.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('../chunks/promotores_B-HGSY0t.mjs');
-
-export { page };
